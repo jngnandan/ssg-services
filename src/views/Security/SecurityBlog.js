b04fab8c34aa90995e8e5a0a0597ca3dc0ab14a3
Girish Nandan <jngnandan@gmail.com>
2022-10-13 21:12:28 +0100

    blog added

--- src/views/Security/SecurityBlog.js
+++ src/views/Security/SecurityBlog.js
@@ -59,7 +59,7 @@ export default function SecurityBlog() {
         <p className='pl-2 font-semibold text-gray-600 pb-0.5'>Security Services</p>
     </button>
     </Link>
-    {/* <img className='' src={img} alt={title} /> */}
+    <img className='' src={img} alt={title} />
     <h1 className='py-5 text-xl md:text-3xl font-semibold text-gray-800'>{title}</h1>
       <p className='text-gray-500'>{content}</p>
     </div>    
