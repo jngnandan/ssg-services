deab77d1554235f79e69570679a156e73a9f4a05
Girish Nandan <jngnandan@gmail.com>
2023-01-27 05:11:35 +0000

    Update ContactUs.js

--- src/views/ContactUs.js
+++ src/views/ContactUs.js
@@ -10,6 +10,8 @@ export class ContactUs extends Component {
             <div className="pl-2 mt-3 text-gray-800 font-medium">
       <p className="text-gray-800"><span className="mt-3">Landline:</span> 020 3900 2489</p>
       <p className="text-gray-800"><span className="mt-3">Email:</span> info@ssgsecurity.co.uk</p>
+      <p className="text-gray-800"><span className="mt-3">Address:</span> Unit 11, 284 Barking Road, E6 3BA</p>
+
       </div>
           <hr className='border-gray-500 my-4 mb-6' />
           
