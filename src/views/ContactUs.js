f2029ba8da1995b2f87177a77f1586c70554890a
jngnandan <jngnandan@gmail.com>
2024-03-22 16:37:40 +0000

    address updated

--- src/views/ContactUs.js
+++ src/views/ContactUs.js
@@ -5,17 +5,32 @@ export class ContactUs extends Component {
     return (
       <div>
               {/* Get in Touch with Us */}
-            <div className='bg-gray-100 px-6 md:px-16 lg:px-44 xl:px-60 py-10'>
+      <div className='bg-gray-100 px-6 md:px-16 lg:px-44 xl:px-60 py-10'>
       <h2 className='text-2xl text-gray-800 font-semibold'>Contact Us</h2>
             <div className="pl-2 mt-3 text-gray-800 font-medium">
       <p className="text-gray-800"><span className="mt-3">Email:</span> info@ssgsecurity.co.uk</p>
       <p className="text-gray-800"><span className="mt-3">Tel:</span>  02039002489 </p>
       <p className="text-gray-800"><span className="mt-3">Tel:</span>  08003688613 </p>
       <p className="text-gray-800"><span className="mt-3">Fax: </span> 02039001936 </p>
-      <p className="text-gray-800"><span className="mt-3">Head Office :</span> "Secure Security  Ltd <br /> \n Suite E, 1st floor Phoenix Building, <br />3 Ajax Works Hertford Rd, <br /> London,IG11 8DY"</p>
 
-      <p className="text-gray-800"><span className="mt-3">Manchester Branch : </span> Secure Security Guards Ltd, <br />No 4.4, 4th Floor, Purple Zone,<br />Universal SquareDevonshire Street North,<br /> Ardwick, Manchester, M12 6JH</p>
-      <p className="text-gray-800"><span className="mt-3">Tel:</span> 0161 974 6513</p>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 items-start mt-6 mb-10'>
+        <div className='border border-solid border-1 border-gray-400 bg-gray-200 rounded shadow-sm p-3'>
+        <p className="text-gray-800"><span className="font-semibold">Head Office:</span> <br/> Secure Security  Ltd <br /> Suite E, 1st floor Phoenix Building, <br />3 Ajax Works Hertford Rd, <br /> London,IG11 8DY</p>
+        <p className="text-gray-800"><span className="">Tel:</span>02039001936</p>
+
+        </div>
+
+        <div className='border border-solid border-1 border-gray-400 bg-gray-200 rounded shadow-sm p-3'>
+        <p className="text-gray-800 "><span className="font-semibold">Manchester Branch :<br/> </span> Secure Security Guards Ltd, <br />No 4, 4th Floor, Purple Zone,<br />North Street,<br /> Ardwick, Manchester, M12 6JH</p>
+        <p className="text-gray-800"><span className="">Tel:</span> 0161 974 6513</p>
+        </div>
+
+        <div className='border border-solid border-1 border-gray-400 bg-gray-200 rounded shadow-sm p-3'>
+        <p className="text-gray-800"><span className="font-semibold">Glasgow Branch :<br/> </span> Secure Security Guards Ltd, <br />Office 32,<br />Stathclyde Business Center,<br /> 120 Carstairs Street, Dalmarnock, Glasgow, G40 4JD</p>
+        {/* <p className="text-gray-800"><span className="">Tel:</span> 0161 974 6513</p> */}
+        </div>
+      </div>
+     
       </div>
           <hr className='border-gray-500 my-4 mb-6' />
           
