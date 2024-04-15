3d79daf66540fc04f6b23c6bbe50c494133ed5ba
srinu-hue <65288521+srinu-hue@users.noreply.github.com>
2024-04-15 17:39:45 +0000

    ERROR

--- src/App.js
+++ src/App.js
@@ -1,58 +1,58 @@
 
-import Home from './views/Home';
-import {BrowserRouter, Routes, Route, redirect, Navigate} from 'react-router-dom';
-import ContactUs from './views/ContactUs';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import TrainingServices from './views/Boxing';
-import CleaningServices from './views/CleaningServices'
+// import Home from './views/Home';
+// import {BrowserRouter, Routes, Route, redirect, Navigate} from 'react-router-dom';
+// import ContactUs from './views/ContactUs';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import TrainingServices from './views/Boxing';
+// import CleaningServices from './views/CleaningServices'
 
-import Boxing from './views/Boxing'
-import SecurityServices from '../src/views/SecurityServices'
-import SecurityPage from './views/Security/SecurityPage';
-import CleaningPage from './views/Cleaning/CleaningPage';
+// import Boxing from './views/Boxing'
+// import SecurityServices from '../src/views/SecurityServices'
+// import SecurityPage from './views/Security/SecurityPage';
+// import CleaningPage from './views/Cleaning/CleaningPage';
 
-import SecurityBlog from './views/Security/SecurityBlog';
-import Policies from './views/Policies';
-
-
-import ReactGA from "react-ga";
-import Thanks from './views/Thanks';
-import { useEffect } from 'react';
-import ViewPolicy from './views/ViewPolicy';
-const TRACKING_ID = "G-YFR289R318"; // OUR_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
+// import SecurityBlog from './views/Security/SecurityBlog';
+// import Policies from './views/Policies';
 
 
-function App() {
+// import ReactGA from "react-ga";
+// import Thanks from './views/Thanks';
+// import { useEffect } from 'react';
+// import ViewPolicy from './views/ViewPolicy';
+// const TRACKING_ID = "G-YFR289R318"; // OUR_TRACKING_ID
+// ReactGA.initialize(TRACKING_ID);
+
+
+// function App() {
 
 
 
-  return (
-    <div className='h-screen mb-auto'>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          {/* <Route path='/' element={<Home />} /> */}
-          <Route path='/clearning-services' element={<CleaningServices />} />
-          <Route path='/contactus' element={<ContactUs />} />
-          <Route path='/boxing' element={<Boxing />} />
-          <Route path='/security-services' element={<SecurityServices />} />
-          <Route path='/security-services/:id' element={<SecurityPage />} />
-          <Route path='/security-services/blog/:id' element={<SecurityBlog />} />
-          <Route
-          path="/"
-          element={<Navigate to="/security-services" replace />}
-          />
-          <Route path='/cleaning-services/:id' element={<CleaningPage />} />
-          <Route path='/thanks' element={<Thanks />} />
-          <Route path='/policies' element={<Policies />} />
-          <Route path='/:id' element={<ViewPolicy />}/>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
-}
+//   return (
+//     <div className='h-screen mb-auto'>
+//       <BrowserRouter>
+//         <Header />
+//         <Routes>
+//           {/* <Route path='/' element={<Home />} /> */}
+//           <Route path='/clearning-services' element={<CleaningServices />} />
+//           <Route path='/contactus' element={<ContactUs />} />
+//           <Route path='/boxing' element={<Boxing />} />
+//           <Route path='/security-services' element={<SecurityServices />} />
+//           <Route path='/security-services/:id' element={<SecurityPage />} />
+//           <Route path='/security-services/blog/:id' element={<SecurityBlog />} />
+//           <Route
+//           path="/"
+//           element={<Navigate to="/security-services" replace />}
+//           />
+//           <Route path='/cleaning-services/:id' element={<CleaningPage />} />
+//           <Route path='/thanks' element={<Thanks />} />
+//           <Route path='/policies' element={<Policies />} />
+//           <Route path='/:id' element={<ViewPolicy />}/>
+//         </Routes>
+//         <Footer />
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
